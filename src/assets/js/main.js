d6fd7bc4bc4b771580e1ae2fad3e5e5536a9d7ef
Vlad <fixedfix921@gmail.com>
2018-03-22 10:14:25 +0200

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+//= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+
+
+
+
+
+
